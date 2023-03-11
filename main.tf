d1ca75e5aba754a424d9381758141457a55108fb
Lars With <lars@with.de>
2023-03-11 23:31:52 +0100

    add package update, upgrade, reboot

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 locals {
   cloud_init_parts_keys_sorted = [
     "cloud_init_start",
+    "cloud_init_package_update",
+    "cloud_init_package_upgrade",
+    "cloud_init_package_reboot_if_required",
     "cloud_init_write_files",
     "cloud_init_write_files_docker",
     "cloud_init_write_files_fail2ban",
@@ -18,22 +21,25 @@ locals {
     "cloud_init_runcmd_rke2_master_other"
   ]
   cloud_init_parts = {
-    cloud_init_start                    = "#cloud-config"
-    cloud_init_write_files              = "write_files:"
-    cloud_init_write_files_docker       = local.cloud_init_docker_write_files
-    cloud_init_write_files_fail2ban     = var.fail2ban ? local.cloud_init_fail2ban_write_files : ""
-    cloud_init_packages                 = "packages:"
-    cloud_init_packages_gettext_base    = local.gettext_base ? local.cloud_init_gettext_base_packages : ""
-    cloud_init_packages_jq              = var.jq ? local.cloud_init_jq_package : ""
-    cloud_init_packages_vault           = local.vault ? local.cloud_init_vault_package : ""
-    cloud_init_packages_fail2ban        = var.fail2ban ? local.cloud_init_fail2ban_package : ""
-    cloud_init_runcmd                   = "runcmd:"
-    cloud_init_runcmd_croc              = var.croc ? local.cloud_init_croc_runcmd : ""
-    cloud_init_runcmd_docker            = var.docker ? local.cloud_init_docker_runcmd : ""
-    cloud_init_runcmd_vault             = local.vault ? local.cloud_init_vault_runcmd : ""
-    cloud_init_runcmd_fail2ban          = var.fail2ban ? local.cloud_init_fail2ban_runcmd : ""
-    cloud_init_runcmd_rke2_master_1st   = var.rke2_master_1st ? local.cloud_init_runcmd_rke2_master_1st : ""
-    cloud_init_runcmd_rke2_master_other = var.rke2_master_other ? local.cloud_init_runcmd_rke2_master_other : ""
+    cloud_init_start                      = "#cloud-config"
+    cloud_init_package_update             = var.package_update ? "package_update: true" : ""
+    cloud_init_package_upgrade            = var.package_upgrade ? "package_upgrade: true" : ""
+    cloud_init_package_reboot_if_required = var.package_reboot_if_required ? "package_reboot_if_required: true" : ""
+    cloud_init_write_files                = "write_files:"
+    cloud_init_write_files_docker         = local.cloud_init_docker_write_files
+    cloud_init_write_files_fail2ban       = var.fail2ban ? local.cloud_init_fail2ban_write_files : ""
+    cloud_init_packages                   = "packages:"
+    cloud_init_packages_gettext_base      = local.gettext_base ? local.cloud_init_gettext_base_packages : ""
+    cloud_init_packages_jq                = var.jq ? local.cloud_init_jq_package : ""
+    cloud_init_packages_vault             = local.vault ? local.cloud_init_vault_package : ""
+    cloud_init_packages_fail2ban          = var.fail2ban ? local.cloud_init_fail2ban_package : ""
+    cloud_init_runcmd                     = "runcmd:"
+    cloud_init_runcmd_croc                = var.croc ? local.cloud_init_croc_runcmd : ""
+    cloud_init_runcmd_docker              = var.docker ? local.cloud_init_docker_runcmd : ""
+    cloud_init_runcmd_vault               = local.vault ? local.cloud_init_vault_runcmd : ""
+    cloud_init_runcmd_fail2ban            = var.fail2ban ? local.cloud_init_fail2ban_runcmd : ""
+    cloud_init_runcmd_rke2_master_1st     = var.rke2_master_1st ? local.cloud_init_runcmd_rke2_master_1st : ""
+    cloud_init_runcmd_rke2_master_other   = var.rke2_master_other ? local.cloud_init_runcmd_rke2_master_other : ""
   }
   cloud_init_parts_sorted = [
     for key in local.cloud_init_parts_keys_sorted : local.cloud_init_parts[key]
---
+++ variables_package.tf
@@ -0,0 +1,17 @@
+variable "package_update" {
+  description = "if cloud-init user data for package_update should be generated"
+  type        = bool
+  default     = true
+}
+
+variable "package_upgrade" {
+  description = "if cloud-init user data for package_upgrade should be generated"
+  type        = bool
+  default     = true
+}
+
+variable "package_reboot_if_required" {
+  description = "if cloud-init user data for package_reboot_if_required should be generated"
+  type        = bool
+  default     = false
+}
