77e57f27f23b569a7d2d98395ba3ef5696c4944b
Lars With <lars@with.de>
2023-03-15 11:40:06 +0100

    refactor docker vars

--- docker.tf
+++ docker.tf
@@ -2,7 +2,7 @@ locals {
   cloud_init_docker_comment            = ["# docker"]
   cloud_init_docker_write_files_prefix = "${path.module}/templates/docker/cloudinit.yml.write_files"
 
-  cloud_init_docker_write_files_daemon_json = var.docker_vars.docker_manipulate_iptables ? [] : [templatefile("${local.cloud_init_docker_write_files_prefix}_daemon_json.tpl", {})]
+  cloud_init_docker_write_files_daemon_json = var.docker_manipulate_iptables ? [] : [templatefile("${local.cloud_init_docker_write_files_prefix}_daemon_json.tpl", {})]
 
   cloud_init_docker_write_files = join(
     "\n",
