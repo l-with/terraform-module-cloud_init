5c5246a741276c0e7441966a82c1f18f0cf2a517
Lars With <lars@with.de>
2023-03-18 13:15:45 +0100

    remove comments

--- certbot.tf
+++ certbot.tf
@@ -21,32 +21,3 @@ locals {
     }] : []
   }
 }
-
-/*
-module "certbot" {
-  count = local.parts_active["certbot"] ? 1 : 0
-
-  source = "./modules/cloud_init_part"
-
-  part = "certbot"
-  packages = concat(
-    [
-      {
-        template = "${path.module}/templates/certbot/${local.yml_packages}.tpl",
-        vars     = {}
-      }
-    ],
-    var.certbot_dns_hetzner ? [
-      {
-        template = "${path.module}/templates/certbot/${local.yml_packages}_certbot_dns_hetzner.tpl",
-        vars     = {}
-      }
-    ]
-    : []
-  )
-  runcmd = var.certbot_dns_hetzner ? [{
-    template = "${path.module}/templates/certbot/${local.yml_runcmd}.tpl",
-    vars     = {}
-  }] : []
-}
-*/
