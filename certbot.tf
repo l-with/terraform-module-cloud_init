46f4c937e981ca168dd6cb09a493fbde4a05ffd3
Lars With <lars@with.de>
2023-03-18 11:45:08 +0100

    remove comment

--- certbot.tf
+++ certbot.tf
@@ -24,31 +24,3 @@ module "certbot" {
     vars     = {}
   }] : []
 }
-
-/*
-locals {
-  cloud_init_comment_certbot        = ["# certbot"]
-  cloud_init_package_certbot_prefix = "${path.module}/templates/certbot/cloudinit.yml.packages"
-
-  cloud_init_package_certbot = join(
-    "\n",
-    local.cloud_init_comment_certbot,
-    compact([
-      templatefile("${local.cloud_init_package_certbot_prefix}.tpl", {}),
-      var.certbot_dns_hetzner ? templatefile("${local.cloud_init_package_certbot_prefix}_certbot_dns_hetzner.tpl", {}) : ""
-    ])
-  )
-}
-
-locals {
-  cloud_init_runcmd_certbot_prefix = "${path.module}/templates/certbot/cloudinit.yml.runcmd"
-
-  cloud_init_runcmd_certbot = join(
-    "\n",
-    local.cloud_init_comment_certbot,
-    compact([
-      var.certbot_dns_hetzner ? templatefile("${local.cloud_init_runcmd_certbot_prefix}.tpl", {}) : ""
-    ])
-  )
-}
-*/
