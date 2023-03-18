ce03e76c198b0b92b84bfb5bd7998b330d9b607f
Lars With <lars@with.de>
2023-03-18 11:46:28 +0100

    remove comment

--- wait_until.tf
+++ wait_until.tf
@@ -1,5 +1,5 @@
 module "wait_until" {
-  count = var.wait_until ? 1 : 0
+  count = local.parts_active["wait_until"] ? 1 : 0
 
   source = "./modules/cloud_init_parts"
 
@@ -9,18 +9,3 @@ module "wait_until" {
     vars     = {}
   }]
 }
-
-/*
-locals {
-  cloud_init_comment_wait_until       = ["# wait_until"]
-  cloud_init_runcmd_wait_until_prefix = "${path.module}/templates/wait_until/cloudinit.yml.runcmd"
-
-  cloud_init_runcmd_wait_until = join(
-    "\n",
-    local.cloud_init_comment_wait_until,
-    [
-      templatefile("${local.cloud_init_runcmd_wait_until_prefix}.tpl", {})
-    ]
-  )
-}
-*/
