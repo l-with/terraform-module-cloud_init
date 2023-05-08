0fec62684e7fa610d9cb83bb5ab7acf25fc95825
Lars With <lars@with.de>
2023-05-08 17:52:31 +0200

    replace "" with null

--- templates/encrypted_packages/cloudinit.yml.runcmd.tpl
+++ templates/encrypted_packages/cloudinit.yml.runcmd.tpl
@@ -1,4 +1,4 @@
   - export SECRET=${secret}
   - >
-    curl --silent --header "${api_header}" "${url}" | openssl enc -aes-256-cbc -pbkdf2 -d -pass env:SECRET | tar xzC /%{ if post_cmd != "" }
+    curl --silent --header "${api_header}" "${url}" | openssl enc -aes-256-cbc -pbkdf2 -d -pass env:SECRET | tar xzC /%{ if post_cmd != null }
   - ${post_cmd}%{ endif }
--- variables_encrypted_packages.tf
+++ variables_encrypted_packages.tf
@@ -7,10 +7,10 @@ variable "encrypted_packages" {
 variable "encrypted_packages_list" {
   description = "the encrypted packages the cloud-init user data should be generated for"
   type = list(object({
-    url        = string // the url to get the package from
-    api_header = string // the header to authorize getting the package
-    secret     = string // the secret to decrypt the package (`openssl enc -aes-256-cbc -pbkdf2`)"
-    post_cmd   = string // the command to be executed after the installing the package
+    url        = string                 // the url to get the package from
+    api_header = string                 // the header to authorize getting the package
+    secret     = string                 // the secret to decrypt the package (`openssl enc -aes-256-cbc -pbkdf2`)"
+    post_cmd   = optional(string, null) // the command to be executed after the installing the package
   }))
   default   = []
   sensitive = true
