a7d8741281e874fd0ab1346139314bc5187366b1
Lars With <lars@with.de>
2023-06-10 08:08:42 +0200

    remove dangling variable for gettext-base

--- gettext-base.tf
+++ gettext-base.tf
@@ -2,9 +2,7 @@ locals {
   gettext_base = !local.parts_active.gettext_base ? {} : {
     runcmd = [{
       template = "${path.module}/templates/gettext-base/${local.yml_runcmd}_install.tpl",
-      vars = {
-        jq_version = var.jq_version
-      }
+      vars     = {}
     }]
   }
 }
