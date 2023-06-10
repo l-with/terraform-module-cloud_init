b3102b23d4c3403b7d97e14fddae0eef7e70991e
Lars With <lars@with.de>
2023-06-10 07:15:06 +0200

    implement install method packages to jq

--- jq.tf
+++ jq.tf
@@ -1,10 +1,18 @@
 locals {
-  jq = !local.parts_active.jq ? {} : {
-    runcmd = [{
-      template = "${path.module}/templates/jq/${local.yml_runcmd}_install.tpl",
-      vars = {
-        jq_version = var.jq_version
-      }
-    }]
-  }
+  jq = !local.parts_active.jq ? {} : merge(
+    {
+      runcmd = (var.jq_install_method != "binary") ? [] : [{
+        template = "${path.module}/templates/jq/${local.yml_runcmd}_install.tpl",
+        vars = {
+          jq_version = var.jq_version
+        }
+      }],
+      packages = (var.jq_install_method != "packages") ? [] : [{
+        template = "${path.module}/templates/${local.yml_packages}.tpl",
+        vars = {
+          package = "jq",
+        }
+      }],
+    }
+  )
 }
