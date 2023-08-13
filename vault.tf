fd1a3fafbfb8c49b1a44c63cf68a838ed8f18d90
Lars With <lars@with.de>
2023-08-13 21:00:35 +0200

    fix vault tls

--- vault.tf
+++ vault.tf
@@ -25,8 +25,8 @@ locals {
     : "${var.vault_home_path}/tls/key.pem"
   )
   vault_tls_client_ca_file = var.vault_tls_client_ca_file != null ? var.vault_tls_storage_raft_leader_ca_cert_file : local.vault_tls_storage_raft_leader_ca_cert_file
-  vault_tls_cert_file      = var.vault_tls_cert_file != null ? var.vault_tls_cert_file : local.vault_tls_storage_raft_leader_ca_cert_file
-  vault_tls_key_file       = var.vault_tls_key_file != null ? var.vault_tls_key_file : local.vault_tls_storage_raft_leader_client_cert_file
+  vault_tls_cert_file      = var.vault_tls_cert_file != null ? var.vault_tls_cert_file : local.vault_tls_storage_raft_leader_client_cert_file
+  vault_tls_key_file       = var.vault_tls_key_file != null ? var.vault_tls_key_file : local.vault_tls_storage_raft_leader_client_key_file
   vault_tls_files = [
     for vault_tls_file in var.vault_tls_files : {
       file_name = replace(
