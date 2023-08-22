d3ac711028c033739d71bbd04bd9e6f3072edbb5
Lars With <lars@with.de>
2023-08-22 21:10:17 +0200

    use local for vault raft values in template

--- vault.tf
+++ vault.tf
@@ -24,9 +24,10 @@ locals {
     ? var.vault_tls_storage_raft_leader_client_key_file
     : "${var.vault_home_path}/tls/key.pem"
   )
-  vault_tls_client_ca_file = var.vault_tls_client_ca_file != null ? var.vault_tls_client_ca_file : local.vault_tls_storage_raft_leader_ca_cert_file
-  vault_tls_cert_file      = var.vault_tls_cert_file != null ? var.vault_tls_cert_file : local.vault_tls_storage_raft_leader_client_cert_file
-  vault_tls_key_file       = var.vault_tls_key_file != null ? var.vault_tls_key_file : local.vault_tls_storage_raft_leader_client_key_file
+  vault_raft_leader_tls_servername = var.vault_raft_leader_tls_servername != null ? var.vault_raft_leader_tls_servername : ""
+  vault_tls_client_ca_file         = var.vault_tls_client_ca_file != null ? var.vault_tls_client_ca_file : local.vault_tls_storage_raft_leader_ca_cert_file
+  vault_tls_cert_file              = var.vault_tls_cert_file != null ? var.vault_tls_cert_file : local.vault_tls_storage_raft_leader_client_cert_file
+  vault_tls_key_file               = var.vault_tls_key_file != null ? var.vault_tls_key_file : local.vault_tls_storage_raft_leader_client_key_file
   vault_tls_files = [
     for vault_tls_file in var.vault_tls_files : {
       file_name = replace(
@@ -145,7 +146,7 @@ locals {
                   vault_storage_raft_leader_ca_cert_file     = local.vault_tls_storage_raft_leader_ca_cert_file,
                   vault_storage_raft_leader_client_cert_file = local.vault_tls_storage_raft_leader_client_cert_file,
                   vault_storage_raft_leader_client_key_file  = local.vault_tls_storage_raft_leader_client_key_file,
-                  vault_raft_leader_tls_servername           = var.vault_raft_leader_tls_servername,
+                  vault_raft_leader_tls_servername           = local.vault_raft_leader_tls_servername,
                   vault_disable_mlock                        = var.vault_disable_mlock,
                 }),
                 write_file_owner = "vault"
