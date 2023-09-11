e8beaae3ed31b7c4c7e561ccbf20594d78830c8c
Lars With <lars@with.de>
2023-09-11 08:15:53 +0200

    add examples to ipv4_address_command

--- variables.tf
+++ variables.tf
@@ -17,7 +17,12 @@ variable "base64_encode" {
 }
 
 variable "ipv4_address_command" {
-  description = "the command to determin the ipv4 address"
+  description = <<EOT
+    the command to determin the ipv4 address, other possible ways are
+      - ip route get 8.8.8.8 | grep 8.8.8.8 | sed -E 's/.*src (\S*) .*/\1/'
+      - ip addr show | grep 'inet ' | grep 'scope global' | cut -d ' ' -f6 | cut -d '/' -f 1 | head -n 1
+      - curl https://ifconfig.me
+  EOT
   type        = string
   default     = "ip addr show | grep 'inet ' | grep 'scope global' | cut -d ' ' -f6 | cut -d '/' -f 1 | head -n 1"
 }
