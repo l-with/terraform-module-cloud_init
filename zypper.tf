d84ad5bdf2e254a84f20845dda9144fe8dba6575
Lars With <lars@with.de>
2023-10-05 12:26:13 +0200

    add zypper refresh

--- zypper.tf
+++ zypper.tf
@@ -1,14 +1,24 @@
 locals {
   zypper = {
-    runcmd = !local.parts_active.zypper ? [] : [
-      for index, zypper_repository in var.zypper_repositories :
-      {
-        template = "${path.module}/templates/zypper/${local.yml_runcmd}_addrepo.tpl",
-        vars = {
-          uri   = zypper_repository.uri,
-          alias = zypper_repository.alias,
-        },
-      }
-    ],
+    runcmd = !local.parts_active.zypper ? [] : concat(
+      [
+        for index, zypper_repository in var.zypper_repositories :
+        {
+          template = "${path.module}/templates/zypper/${local.yml_runcmd}_addrepo.tpl",
+          vars = {
+            uri   = zypper_repository.uri,
+            alias = zypper_repository.alias,
+          },
+        }
+      ],
+      [
+        {
+          template = "${path.module}/templates/${local.yml_runcmd}_runcmd.tpl",
+          vars = {
+            runcmd_script = "  - zypper --gpg-auto-import-keys refresh"
+          }
+        }
+      ],
+    )
   }
 }
