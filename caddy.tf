85224af98d8080d48fe6149ecb50b20a1ddc948f
Lars With <lars@with.de>
2025-08-05 20:19:24 +0200

    implement caddy apt install

--- caddy.tf
+++ caddy.tf
@@ -10,21 +10,27 @@ locals {
     },
     var.caddy_configuration == null ? {} : {
       runcmd = concat(
-        var.caddy_configuration == null ? [] : [{
-          template = "${path.module}/templates/caddy/${local.yml_runcmd}_write_file.tpl"
-          vars = {
-            write_file_directory = "/etc/caddy"
-            write_file_name      = "Caddyfile"
-            write_file_mode      = "644"
-            write_file_content = templatefile("${path.module}/templates/caddy/Caddyfile.tpl", {
-              caddy_configuration = var.caddy_configuration
-            })
+        var.caddy_configuration == null ? [] : [
+          {
+            template = "${path.module}/templates/caddy/${local.yml_runcmd}_write_file.tpl"
+            vars = {
+              write_file_directory = "/etc/caddy"
+              write_file_name      = "Caddyfile"
+              write_file_mode      = "644"
+              write_file_content = templatefile("${path.module}/templates/caddy/Caddyfile.tpl", {
+                caddy_configuration = var.caddy_configuration
+              })
+            }
+          },
+          {
+            template = "${path.module}/templates/caddy/${local.yml_runcmd}_install.tpl",
+            vars = {}
+          },
+          {
+            template = "${path.module}/templates/caddy/${local.yml_runcmd}_restart.tpl",
+            vars     = {}
           }
-        }],
-        [{
-          template = "${path.module}/templates/caddy/${local.yml_runcmd}_restart.tpl",
-          vars     = {}
-        }],
+        ],
       )
     }
   )
