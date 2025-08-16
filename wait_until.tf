7ab45ad0c7e2649b0586516a989f625b1884cdd0
Lars With <lars@with.de>
2025-08-16 12:20:37 +0200

    remove wait untils scripts

--- wait_until.tf
+++ wait_until.tf
@@ -7,19 +7,6 @@ locals {
           vars     = {}
         }
       ],
-      [for wait_until_script in var.wait_until_scripts :
-        {
-          template = "${path.module}/templates/wait_until/${local.yml_runcmd}_write_file_script.tpl",
-          vars = {
-            write_file_directory = "/root",
-            write_file_name      = wait_until_script.file_name,
-            write_file_content   = wait_until_script.content,
-            write_file_owner     = "root"
-            write_file_group     = "root"
-            write_file_mode      = "755",
-          }
-        }
-      ],
     )
   }
 }
